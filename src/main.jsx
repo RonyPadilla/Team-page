61c091fc4872f42ebc4649724efaf9eec6752472
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-07-03 21:57:43 -0600

    First commit in the project

---
+++ src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <header>
+      <h1>The creative crew</h1>
+    </header>
+  </React.StrictMode>,
+)
