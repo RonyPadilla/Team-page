c4af454f7cd34107101f705e94cfdccba5a38bc1
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-07-08 22:29:14 -0600

    add plus css

--- src/main.jsx
+++ src/main.jsx
@@ -4,13 +4,11 @@ import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <header className="header-info">
+    <header className="header-title">
+        <h1 className="h1-title">The creative crew</h1>
       <div>
-        <h1>The creative crew</h1>
-      </div>
-      <div>
-        <h2>Who we are</h2>
-        <p>
+        <h2 className="h2-title">Who we are</h2>
+        <p className="p-title">
           We are team of creatively diverse. driven. innovative individuals
           working in various locations from the world.
         </p>
@@ -26,10 +24,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">Product owner</p>
         </div>
-
         <h2>Bill Mahoney</h2>
       </div>
-      <div className="div-person-half">
+
+      <div className="div-person">
         <div className="img-text-info">
           <img
             className="img-person-2"
@@ -38,9 +36,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">Art director</p>
         </div>
-
         <h2>Saba Cabrera</h2>
       </div>
+
       <div className="div-person">
         <div className="img-text-info">
           <img
@@ -50,9 +48,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">Tech Lead</p>
         </div>
-
         <h2>Shae Le</h2>
       </div>
+
       <div className="div-person">
         <div className="img-text-info">
           <img
@@ -62,10 +60,10 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">UX Designer</p>
         </div>
-
         <h2>Skylah Lu</h2>
       </div>
-      <div className="div-person-half">
+
+      <div className="div-person">
         <div className="img-text-info">
           <img
             className="img-person-5"
@@ -74,9 +72,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">Developer</p>
         </div>
-
         <h2>Griff Richards</h2>
       </div>
+
       <div className="div-person">
         <div className="img-text-info">
           <img
@@ -86,9 +84,9 @@ ReactDOM.createRoot(document.getElementById("root")).render(
           />
           <p className="p-info">Developer</p>
         </div>
-
         <h2>Stan John</h2>
       </div>
+
     </main>
     <footer className="footer-credits">
       <p>
